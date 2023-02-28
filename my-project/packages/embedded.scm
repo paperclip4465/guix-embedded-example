bca20f80241b6fc66bc1a15084456f6311e7d112
Mitchell Schmeisser <mitchellschmeisser@librem.one>
2023-02-28 13:05:20 -0500

    Added image signing phase to firmware.
    Now the output of k64f-temp-firmware can be flashed to 0x20000
    and be booted by k64f-bootloader flashed at 0x0.

--- my-project/packages/embedded.scm
+++ my-project/packages/embedded.scm
@@ -33,38 +33,56 @@
 		    zcbor
 		    zephyr-mcuboot))
       (arguments
-       `(#:zephyr zephyr-3.1
-	 #:configure-flags '("-DBOARD=frdm_k64f")))
+       `(#:zephyr ,zephyr-3.1
+	 #:bin-name "k64-temp"
+	 #:board "frdm_k64f"))
       (synopsis "Temperature measurement firmware for k64f")
       (description "This firmware measures the temperature every 60 seconds
 and publishes it over MQTT. In addition it also implements an SMP server over
-UDP that can be used for firmware update/device controll.")
+UDP that can be used for firmware update/device control.")
       (license license:gpl3+))))
 
 (define-public k64f-temp-firmware
   (let ((base k64f-temp-firmware-stand-alone))
     (package (inherit base)
+      (name "k64f-temp-firmware")
       (arguments
-       (substitute-keyword-arguments base
-	 ((#:configure-flags flags)
-	  `(append '("-DCONFIG_BOOTLOADER_MCUBOOT=y")
-		   ,flags))))
-      (description
-       (string-append (package-description base)
-		      "This firmware is linked against the primary
-image slot and must be loaded by mcuboot. (see device tree)")))))
-
+       `(#:configure-flags '("-DCONFIG_BOOTLOADER_MCUBOOT=y"
+			     "-DCONFIG_ROM_START_OFFSET=0x200")
+	 #:phases
+	 (modify-phases %standard-phases
+	   ;; Replace output binary with signed version.
+	   (add-before 'install 'sign-firmware
+	     (lambda* (#:key inputs #:allow-other-keys)
+	       (let ((bin "zephyr/zephyr.bin")
+		     (unsigned "zephyr/zephyr.unsigned.bin")
+		     (key (assoc-ref inputs "signing-key")))
+		 (format #t "Signing firmware with key ~a~&" key)
+		 (copy-file bin unsigned)
+		 (system* "imgtool" "sign"
+			  "--key" key
+			  "--align" "4"
+			  "--header-size" "0x200"
+			  "--slot-size" "0x60000"
+			  "--version" "0.0.0"
+			  "--confirm"
+			  unsigned bin)))))
+	 ,@(package-arguments base)))
+      (native-inputs
+       (append `(("signing-key" ,%firmware-signing-key)
+		 ("imgtool" ,imgtool))
+	       (package-native-inputs base)))
+      (description (string-append "Signed firmware suitable for MCUBOOT\n\n"
+				  (package-description base))))))
 
 (define-public k64f-bootloader
-  (let ((mcuboot (make-mcuboot "frdm_k64f"
-			       ;; Use special dev key instead of production
-			       (local-file "../../dev.pem")
+  (let ((mcuboot (make-mcuboot "frdm_k64f" %firmware-signing-key
 			       #:extra-zephyr-modules (list hal-cmsis hal-nxp)
 			       #:extra-configure-flags
-			       '(;; k64 doesn't have fancy crypto hardware
+			       '( ;; k64 doesn't have fancy crypto hardware
 				 ;; so we cannot use RSA keys.
 				 "-DCONFIG_BOOT_SIGNATURE_TYPE_ECDSA_P256=y"
 				 "-DCONFIG_BOOT_SIGNATURE_TYPE_RSA=n"
 				 "-DCONFIG_BOOT_ECDSA_TINYCRYPT=y"))))
     (package (inherit mcuboot)
-	     (name "k64f-bootloader"))))
+      (name "k64f-bootloader"))))
